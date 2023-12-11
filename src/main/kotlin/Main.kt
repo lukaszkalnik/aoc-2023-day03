22b2cd5147456fed04d01acec16784b0447fb0a6
Lukasz Kalnik <lukasz.kalnik@gmail.com>
2023-12-11 20:10:43 +0100

    Parse indexed numbers

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,43 @@
 package org.example
 
+import java.io.File
+
 fun main() {
-    println("Hello World!")
+    val input = File("input.txt").readLines()
+
+    val numbersWithPositions = input.first().parseNumbersWithPositions()
+    println(numbersWithPositions)
+}
+
+private fun String.parseNumbersWithPositions(): List<IndexedValue<Int>> {
+    val numbersWithPositions = mutableListOf<IndexedValue<Int>>()
+    val iterator = iterator().withIndex()
+
+    while (iterator.hasNext()) {
+        var numberString = ""
+        var index = 0
+        val indexedChar = iterator.next()
+
+        if (indexedChar.value.isDigit()) {
+            numberString += indexedChar.value
+            index = indexedChar.index
+
+            while (iterator.hasNext()) {
+                val nextIndexedChar = iterator.next()
+                if (nextIndexedChar.value.isDigit()) {
+                    numberString += nextIndexedChar.value
+                } else {
+                    numbersWithPositions.add(
+                        IndexedValue(
+                            value = numberString.toInt(),
+                            index = index,
+                        )
+                    )
+                    break
+                }
+            }
+        }
+    }
+
+    return numbersWithPositions
 }
